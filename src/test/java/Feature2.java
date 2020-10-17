96ca22c8f3ba674dab568477caac8cee41f850f1
Mehmet Sait DEMİR <demirmsait@gmail.com>
2020-10-17 21:01:46 +0300

    Feature 2 more change for commit

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -1,6 +1,11 @@
 public class Feature2 {
     public static void main(String[] args) {
         System.out.println("Feature 2 is nice");
+        System.out.println("Feature 2 second commit");
+        System.out.println("Feature 2 second commit");
+        System.out.println("Feature 2 second commit");
+        System.out.println("Feature 2 second commit");
+        System.out.println("Feature 2 second commit");
 
 
     }
