44e50cdc320f1ceb2c3197d971a8eafdf6ddfec8
mehmetsaitdemir <72044706+mehmetsaitdemir@users.noreply.github.com>
2020-10-18 01:57:55 +0300

    added changes to hello from Github

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println(" still awesome Hello");
 
-        System.out.println("Really hello");
+        System.out.println("Really hello we can make chnages from git hub ");
     }
 }
