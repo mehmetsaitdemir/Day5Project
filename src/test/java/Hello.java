c1fa8ddb4ca37bbf0e89fa4a99bd2826ed2d37fe
Mehmet Sait DEMİR <demirmsait@gmail.com>
2020-10-18 01:15:46 +0300

    added more changes

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,8 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println(" still awesome Hello");
 
-        System.out.println("Really hello");
-        // add one more line on your local and commit
-        // new co worker
+        System.out.println("This is my own hello");//this is my local hello and committed
+
     }
 }
